d204dd6a5b5890ad39ecf4bf528c00303b6b8200
firegloves <lucorset@gmail.com>
2021-05-13 12:47:36 +0200

    test rationalisation

---
+++ rationalised/functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function setGitCredentials() {
+
+  email=$1
+  username=$2
+
+  echo "##### setGitCredentials START ####"
+  echo "##### email ${email} ####"
+  echo "##### name ${username} ####"
+#  git config --global user.email ${email}
+#  git config --global user.name ${username}
+  echo "##### setGitCredentials END ####"
+
+}
+
+
